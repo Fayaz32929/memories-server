e7232c521ca537febd9894b16998da206a7651d8
fayaz <lagharifayazali3@gmail.com>
2023-12-06 14:23:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import PostRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/api/posts", PostRoutes);
+app.get("/", (req, res) => {
+ res.send("hello to mmemories api");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+ .connect(process.env.CONNECTION_URL)
+ .then(() => app.listen(PORT, () => console.log(`server Running on port ${PORT}`)))
+ .catch((err) => console.log(err));
+
+// mongoose.set("useFindAndModify", false);
