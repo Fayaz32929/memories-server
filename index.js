f6908ac3e8359daede3d07b377fab59bd984d954
Fayaz32929 <112559827+Fayaz32929@users.noreply.github.com>
2023-12-06 22:22:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/posts", PostRoutes);
+app.use("/api", PostRoutes);
 
 const PORT = process.env.PORT || 5000;
 
